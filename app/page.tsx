d5405396b62f8e1b030bbd30c0a557792862670a
Alex Bogut <alexbogut11@gmail.com>
2022-12-30 18:46:11 -0500

    change for fun

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div>This is the home Page</div>;
+  return <div>HOME PAGE</div>;
 };
 
 export default page;
